67100946e616fbdc776c9c0e63a9b521ace4f4e6
Krzychu159 <krzysztof.rak159@gmail.com>
2025-03-17 20:15:45 +0100

    Update

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const shareButton = document.querySelector("#share");
+const shareButtonOff = document.querySelector("#shareOff");
 shareButton.addEventListener("click", () => {
   document.querySelector(".share-popup").classList.toggle("active");
-  console.dir(document.querySelector(".share-popup"));
+  document.querySelector(".author").classList.toggle("active");
+});
+shareButtonOff.addEventListener("click", () => {
+  document.querySelector(".share-popup").classList.toggle("active");
+  document.querySelector(".author").classList.toggle("active");
 });
